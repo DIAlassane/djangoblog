adce4ec99fee978e32e9ff8a28a4a4de2aca2797
DIAlassane <alassdia0909@gmail.com>
2024-06-29 22:46:38 +0200

    commit-rd

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mpoeme.onrender.com', 'localhost', '127.0.0.1', '[::1]']
 
 # Application definition
 
